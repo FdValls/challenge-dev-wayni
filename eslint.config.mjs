f291a5d3c6787762a78ca3dc070e417f464554fe
AzureAD\FernandoDanielValls <fernando.valls@vusecurity.com>
2025-07-01 00:20:22 -0300

    documentación lista

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-    // ...compat.extends("next/core-web-vitals", "next/typescript"),
-
     ...compat.config({
         extends: [
             "next/core-web-vitals",
@@ -21,30 +19,29 @@ const eslintConfig = [
         ],
         plugins: ["prettier"],
         rules: {
-            "@typescript-eslint/no-explicit-any": "off", // Valida uso de tipo "any"
+            "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unused-vars": "warn",
-            "react-hooks/rules-of-hooks": "off", // Valida el orden correcto de los hooks
+            "react-hooks/rules-of-hooks": "off",
             "prefer-const": ["warn"],
-            quotes: "off", // Uso de comillas, simple o dobles
+            quotes: "off",
             "prettier/prettier": [
                 "warn",
                 {
-                    printWidth: 100, // Líneas más largas se ajustan automáticamente
-                    endOfLine: "auto", // Establece el estilo de salto de línea
-                    semi: true, // Define si el código debe tener punto y coma al final de cada línea "Opcional"
-                    tabWidth: 4, // Establece la cantidad de espacios por tabulación
-                    jsxBracketSameLine: false, // Los paréntesis de cierre irán en nueva línea
-                    jsxSingleQuote: false, // Usa comillas dobles para atributos JSX
-                    singleAttributePerLine: false, // Permite múltiples atributos en la misma línea
-                    bracketSpacing: true, // Espacios entre llaves
-                    bracketSameLine: false, // Los paréntesis de cierre irán en nueva línea
+                    printWidth: 100,
+                    endOfLine: "auto",
+                    semi: true,
+                    tabWidth: 4,
+                    jsxBracketSameLine: false,
+                    jsxSingleQuote: false,
+                    singleAttributePerLine: false,
+                    bracketSpacing: true,
+                    bracketSameLine: false,
                     useTabs: false,
                 },
             ],
-            // indent: ["warn", 4, { SwitchCase: 2 }],
+
             indent: "off",
-        }, // Usado por esLint, si la identación es diferente,
-        // genera advertencia 4 espacios para la indentación
+        },
     }),
 ];
 
